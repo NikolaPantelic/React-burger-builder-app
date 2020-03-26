6f9f3dd4cde01820b0fbe88712a014b61a338e8e
Nikola Pantelic <0nikolapantelic0@gmail.com>
2020-03-26 12:16:36 +0100

    controls adding

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,24 @@ import styles from "./Burger.module.css";
 import Burgeringredient from "./Burgeringredient/Burgeringredient";
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <Burgeringredient type={igKey} key={igKey + i}/>;
+            });
+        }).reduce((arr, el) => {return arr.concat(el)},[]);
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients !!!</p>
+    }
+
 
 
     return (
         <div className={styles.Burger}>
-           <Burgeringredient type={"bread-top"}/>
-           <Burgeringredient type={"cheese"}/>
-           <Burgeringredient type={"meat"}/>
-           <Burgeringredient type={"bread-bottom"}/>
+            <Burgeringredient type={"bread-top"}/>
+            {transformedIngredients}
+            <Burgeringredient type={"bread-bottom"}/>
         </div>
     );
 };
